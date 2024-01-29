7127d47c743879479c728d87c7d6d7084dca7565
samuelemde <samuel.emde@gmail.com>
2024-01-29 09:57:10 +0100

    feat: update handlers

--- storyblok/YeFormDisplay.vue
+++ storyblok/YeFormDisplay.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import type { StoryblokStory } from "storyblok-generate-ts";
-import { type Email, YeForm, type YeFormConfig } from "@yoe-form/nuxt";
+import {
+  type EmailHandler,
+  type RestHandler,
+  YeForm,
+  type YeFormConfig,
+} from "@yoe-form/nuxt";
 import { ref } from "vue";
 
 type YeFormDisplay = {
@@ -33,15 +38,11 @@ const { data } = (await storyblokApi.get(`cdn/stories/${blok.form}`, {
 })) as { data: { story: StoryblokStory<YeFormBlok> } };
 
 config.value = data.story.content.yeForm;
-const handleEmail = async (
-  values: any,
-  internalEmail: Email,
-  confirmationEmail?: Email,
-) => {
-  alert(JSON.stringify(values));
+const handleEmail: EmailHandler = async (values, emails) => {
+  alert(JSON.stringify(emails));
 };
 
-const handleRestCall = async (url: string, values: any) => {
+const handleRestCall: RestHandler = async (url: string, values: any) => {
   alert(JSON.stringify({ url, ...values }));
 };
 </script>
